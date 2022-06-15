5bb8724bad187c664d6e31ac6eb0f8a9a3e02675
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-06-15 10:24:22 +0300

    sliders

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    speed: 600,
+});
