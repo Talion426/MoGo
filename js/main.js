cd3559fc9704375a163a7dff15ab2cff58e7718d
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-07-15 17:12:47 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ const swiper = new Swiper(".swiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
     loop: true,
     speed: 600,
 });
