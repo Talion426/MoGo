bc8fbecc4f9ab8e5216f8cd909a5b653bf4fc634
Talion426 <106770432+Talion426@users.noreply.github.com>
2022-10-02 03:28:00 +0300

    added js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,38 @@ const swiper = new Swiper(".swiper", {
     loop: true,
     speed: 600,
 });
+
+let burgerButton = document.querySelector(".header-nav__burger-btn");
+
+let burgerMenu = document.querySelector(".burger-menu");
+
+burgerButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    burgerMenu.classList.toggle("_active");
+    document.body.classList.toggle("_fixed");
+});
+
+const menuLinks = document.querySelectorAll("[data-goto]");
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach((menuLink) => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        if (burgerMenu.classList.contains("_active")) {
+            burgerMenu.classList.remove("_active");
+            document.body.classList.remove("_fixed");
+        }
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth",
+            });
+            e.preventDefault();
+        }
+    }
+}
